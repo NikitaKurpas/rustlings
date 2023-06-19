3b9d8ece986e9100e59b19f28541535a5a9e3431
Nikita Kurpas <nikitakurpas@gmail.com>
2023-06-19 08:07:36 +0000

    fix(exercises): complete "threads" and "smart_pointers"

---
+++ exercises/smart_pointers/arc2.rs
@@ -0,0 +1,22 @@
+// arc2.rs
+
+#![forbid(unused_imports)] // Do not change this, (or the next) line.
+use std::sync::Arc;
+use std::thread;
+
+fn main() {
+    let numbers: Vec<_> = (0..80u32).collect();
+    let shared_numbers = Arc::new(numbers);
+    let mut joinhandles = Vec::new();
+
+    for offset in 0..8 {
+        let child_numbers = Arc::clone(&shared_numbers[offset * 10..(offset + 1) * 10]);
+        joinhandles.push(thread::spawn(move || {
+            let sum: u32 = child_numbers.iter().sum();
+            println!("Sum of offset {} is {}", offset, sum);
+        }));
+    }
+    for handle in joinhandles.into_iter() {
+        handle.join().unwrap();
+    }
+}
